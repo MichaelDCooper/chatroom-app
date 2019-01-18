2d353a9838db2e3f11b31c5fdd8b648f62569b7b
Michael Cooper <mcooper628@gmail.com>
2019-01-17 23:36:50 -0800

    Finished Chatroom List

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import logo from './logo.svg';
 import './App.css';
 import RoomList from './components/RoomList';
 
-var config = {
-  apiKey: "AIzaSyBxVQoVC1HkJMS9XqEWawJFQi63cGM4JiQ",
-  authDomain: "chatroom-app-67193.firebaseapp.com",
-  databaseURL: "https://chatroom-app-67193.firebaseio.com",
-  projectId: "chatroom-app-67193",
-  storageBucket: "chatroom-app-67193.appspot.com",
-  messagingSenderId: "978067687202"
-};
-firebase.initializeApp(config);
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxVQoVC1HkJMS9XqEWawJFQi63cGM4JiQ",
+    authDomain: "chatroom-app-67193.firebaseapp.com",
+    databaseURL: "https://chatroom-app-67193.firebaseio.com",
+    projectId: "chatroom-app-67193",
+    storageBucket: "chatroom-app-67193.appspot.com",
+    messagingSenderId: "978067687202"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <main>
-          <RoomList firebase = {firebase} />
-        </main>
+        <RoomList firebase = {firebase} />
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,21 +3,24 @@ import React, {Component} from 'react';
 class RoomList extends Component{
   constructor(props){
     super(props);
+
     this.state = {
       rooms: [],
-    }
+    };
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
+
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
     const room = snapshot.val();
     room.key = snapshot.key;
     this.setState({ rooms: this.state.rooms.concat( room ) })
   });
+
 }
   render(){
     return (
-      <div>
+      <div className = "App">
         <ul>
             {this.state.rooms.map((room,index) =>
               <li className = "room" key = {index} >
